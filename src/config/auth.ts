3c330a68d6d39896e8ed680701f623f6f1cbfd41
Nataniel Porto <natanielkporto@gmail.com>
2020-11-10 02:10:00 -0300

    added several functionalities - till avatar

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '224662156e3e65b1863ca37e3c493714',
+    expiresIn: '1d',
+  },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,9 +5,14 @@ import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthentication from '../middlewares/ensureAuthentication';
+
 const appointmentsRouter = Router();
 
+appointmentsRouter.use(ensureAuthentication);
+
 appointmentsRouter.get('/', async (request, response) => {
+  console.log(request.user);
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
   const appointments = await appointmentsRepository.find();
@@ -16,7 +21,7 @@ appointmentsRouter.get('/', async (request, response) => {
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -24,7 +29,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
     return response.json(appointment);
   } catch (err) {
